817651b3bfb2ec8f5143e0189c69b256d6132892
Travis Clawson <Travis.Clawson@unc.edu>
2025-01-16 16:28:58 -0500

    Finished Exercise 00!

--- welcome.py
+++ welcome.py
@@ -5,3 +5,14 @@ __author__ = "01234567"
 print("Welcome to COMP110!")
 print("You are in for a fun adventure into programming!")
 print("<3 the COMP110 Team!")
+from random import random
+
+random()
+type(random())
+from random import choice
+
+choice("wxyz")
+type(choice("wxyz"))
+"comp110".isalpha()
+"110".isdigit()
+"comp110"[0].isalpha()
